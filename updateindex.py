47a133ecdffacf2c69ee717a8903cfbc0738915b
mkguhan <mkguhan@gmail.com>
2021-06-05 23:29:43 +0200

    comments updated

--- updateindex.py
+++ updateindex.py
@@ -21,10 +21,11 @@ if __name__ == "__main__":
     update_index = update_index_details()
     print("Getting the list of stocks")
     all_index_list = update_index._download_index_details()
-    all_index_list[' DATE OF LISTING'] = pd.to_datetime(all_index_list['DATE OF LISTING'], format='%d-%m-%Y')
+
     all_index_list.index = all_index_list.index + 1
     all_index_list.columns = all_index_list.columns.str.replace(' ','_')
     all_index_list.columns = all_index_list.columns.str.lstrip('_')
+    all_index_list['DATE_OF_LISTING'] = pd.to_datetime(all_index_list['DATE_OF_LISTING'], format='%d-%m-%Y')
     for row in all_index_list.iterrows():
         values = [row[0], row[1].SYMBOL, row[1].NAME_OF_COMPANY,' ',row[1].DATE_OF_LISTING.strftime("%d-%m-%Y"),row[1].SERIES,row[1].ISIN_NUMBER,row[1].FACE_VALUE,row[1].PAID_UP_VALUE,row[1].MARKET_LOT]
         print(update_index.index_column)
