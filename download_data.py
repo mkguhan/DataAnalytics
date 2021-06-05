905456bbc5ffc22682997b5ee1d30ac5174ebd18
mkguhan <mkguhan@gmail.com>
2021-06-05 21:55:00 +0200

    comments updated

--- download_data.py
+++ download_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 class download_data_nse:
 
     def __init__(self):
-        self.url = "https://www.nseindia.com/"
+        self.url = ""
 
     @staticmethod
     #download the data as Dataframe
@@ -14,14 +14,4 @@ class download_data_nse:
         return read_csv
 
 
-    @staticmethod
-    def _download_url(url):
-        print("Download url")
-        req = requests.get(url)
-        url_content = req.content
-        csv_file = open('index_list.csv', 'wb')
-        file_name = "index_list.csv"
-        csv_file.write(url_content)
-        csv_file.close()
-        return file_name
 
--- updateindex.py
+++ updateindex.py
@@ -4,19 +4,16 @@ from download_data import download_data_nse
 class update_index_details:
 
     def __init__(self):
-        self.index = True
-        self.url = "https://archives.nseindia.com/content/equities/EQUITY_L.csv"
+        self.index_file_path = "files/EQUITY_L.csv"
 
-    def _download_index_details(self, url):
-        download_csv = download_data_nse._download_url(url)
-        index_list = download_data_nse._download_url_as_dataframe(download_csv)
+    def _download_index_details(self):
+        index_list = download_data_nse._download_url_as_dataframe(self.index_file_path)
         return index_list
-
 
 
 
 if __name__ == "__main__":
     update_index = update_index_details()
     print("Getting the list of stocks")
-    all_index_list = update_index._download_index_details(update_index.url)
+    all_index_list = update_index._download_index_details()
     print(all_index_list)
