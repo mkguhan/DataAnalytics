9228ffd2f741a66d7a50f9df07dfe005c7a60326
mkguhan <mkguhan@gmail.com>
2021-06-05 23:11:52 +0200

    comments updated

--- updateindex.py
+++ updateindex.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     all_index_list.columns = all_index_list.columns.str.replace(' ','_')
     all_index_list.columns = all_index_list.columns.str.lstrip('_')
     for row in all_index_list.iterrows():
-        values = [row.[0], row[1].SYMBOL, row[1].NAME_OF_COMPANY,row[1].SERIES,row[1].DATE_OF_LISTING,row[1].PAID_UP_VALUE,row[1].MARKET_LOT]
+        values = [row[0], row[1].SYMBOL, row[1].NAME_OF_COMPANY,row[1].SERIES,row[1].DATE_OF_LISTING,row[1].PAID_UP_VALUE,row[1].MARKET_LOT]
         print(values)
