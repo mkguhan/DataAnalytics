b48550324fa643cc7c4cc61f420d528fb2de0a2e
mkguhan <mkguhan@gmail.com>
2021-06-07 23:29:51 +0200

    updated the code

--- preopen_marketdata.py
+++ preopen_marketdata.py
@@ -34,7 +34,7 @@ class preopenmarket_update:
         )
 
         response = requests.get('https://www.nseindia.com/api/market-data-pre-open', headers=headers, params=params)
-        print(response.text.splitlines())
+        print(response.text.splitlines()[20])
 
 if __name__ == "__main__":
     security_preopen = preopenmarket_update()
